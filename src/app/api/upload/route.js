d63f8739f5caf6c5205e58950532d429955aa4b8
Kawsar Ahmed <ahmed.official2211@gmail.com>
2025-08-30 14:38:53 +0100

    Added amazons3 cons=figuration not working still but will look into it

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,11 +1,11 @@
 import {json} from "node:stream/consumers";
 import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
-
+import uniqid from "uniqid";
 export async function POST(req) {
     const data = await req.formData();
     console.log(data);
-    if(data.get("file")){
-        const file = data.get("file");
+    if(data.get("files")){
+        const file = data.get("files");
 
         const s3Client = new S3Client({
             region: "us-east-1",
@@ -16,11 +16,30 @@ export async function POST(req) {
         });
 
         const ext = file.name.split(".").slice(-1)[0];
+        console.log("EXT in profile/page.js",ext);
+        const newFileName = uniqid() + "." + ext;
+        console.log(newFileName);
 
+        const chunks =[];
+        for await (const chunk of file.stream()){
+            chunks.push(chunk);
+        }
+
+        const buffer = Buffer.concat(chunks);
+        console.log("buffer",buffer);
+
+        const bucket = "food-orderring";
         s3Client.send(new PutObjectCommand({
-            Bucket: "food-orderring",
-            Key: ""
+            Bucket: bucket,
+            Key: newFileName,
+            ACL: "public-read",
+            ContentType: file.type,
+            Body: buffer,
         }))
+
+        const link = "https://"+bucket+ "food-orderring.s3.amazonaws.com/"+newFileName;
+        console.log("Link",link);
+        return Response.json(link);
     }
     return Response.json(true);
 }
